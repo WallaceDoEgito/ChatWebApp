00afb2b1db35f6e1c01e1a704a27abc350518575
Wallace <wallacegomes6877@gmail.com>
2025-05-28 12:30:23 -0300

    fix:createdAt column mapped

---
+++ Backend/ChatApp/Migrations/20250528152612_fixDate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+        }
+    }
+}
--- Backend/ChatApp/Models/User.cs
+++ Backend/ChatApp/Models/User.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using ChatApp.Interfaces;
 using ChatApp.Services;
 
@@ -6,22 +8,25 @@ namespace ChatApp.Models;
 public class User
 {
     public Guid Id { get; set; }
-    public required string UserName { get; set; }
-    public required string PasswordHashed { get; set; }
-    public DateTime CreatedAt = DateTime.Now;
+    [MaxLength(32)] public required string UserName { get; set; } = String.Empty;
+    public required string PasswordHashed { get; set; } = String.Empty;
+    public DateTime CreatedAt { get; set; }= DateTime.Now;
     public ICollection<Channel> Channels { get; set; } = new List<Channel>();
     public ICollection<User> Friends { get; set; } = new List<User>();
     public ICollection<User> FriendOf { get; set; } = new List<User>();
 
-public ICollection<Message> SendMessages { get; set; } = new List<Message>();
+    public ICollection<Message> SendMessages { get; set; } = new List<Message>();
     private readonly IHasher _passwordHasher = new BcryptPasswordHasher();
 
     public User(){}
+    
 
+    [SetsRequiredMembers]
     public User(string username, string password)
     {
         Id = Guid.NewGuid();
         UserName = username;
         PasswordHashed = _passwordHasher.HashPassword(password);
+        CreatedAt = DateTime.Now;
     }
 }
