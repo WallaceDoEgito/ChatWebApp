0ef3e91f6910958c5f8d63044478c5017f538239
Wallace <wallacegomes6877@gmail.com>
2025-06-05 19:32:19 -0300

    feat: routes

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
+import { ChatViewComponent } from './components/chat-view/chat-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    {
+        path:'',
+        redirectTo: 'auth',
+        pathMatch: 'full'
+    },
+    {
+        path:'auth',
+        component:AuthComponent
+    },
+    {
+        path:'hub',
+        component:ChatViewComponent
+    }
+];
