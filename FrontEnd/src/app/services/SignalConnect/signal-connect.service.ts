74afc967c2a7cb715fa323df7e93a070ebc22921
Wallace <wallacegomes6877@gmail.com>
2025-06-18 20:48:21 -0300

    feat: signalR websocket method to send friend req

--- FrontEnd/src/app/services/SignalConnect/signal-connect.service.ts
+++ FrontEnd/src/app/services/SignalConnect/signal-connect.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as signalR from "@microsoft/signalr"
 import {ReplaySubject} from "rxjs";
 
@@ -7,7 +7,7 @@ import {ReplaySubject} from "rxjs";
 })
 export class SignalConnectService {
   private token: any = localStorage.getItem("JWTSession");
-  private Connection = new signalR.HubConnectionBuilder().configureLogging(signalR.LogLevel.Debug).withUrl("http://localhost:5269/chat", {accessTokenFactory: () => this.token}).withAutomaticReconnect().build();
+  private Connection = new signalR.HubConnectionBuilder().configureLogging(signalR.LogLevel.Debug).withUrl("http://localhost:5269/chat", {skipNegotiation:true,transport:signalR.HttpTransportType.WebSockets,withCredentials:true,accessTokenFactory: () => this.token}).withAutomaticReconnect().build();
   private ConnectionSubject = new ReplaySubject<void>(1);
 
   IsConnected$() {
@@ -23,11 +23,15 @@ export class SignalConnectService {
   TryConnect(): Promise<void> {
     return this.Connection.start()
   }
-  
+
   public async GetChannels()
   {
-    const ChannelList = await this.Connection.invoke("GetChannelList");
-    return ChannelList;
+    return await this.Connection.invoke("GetChannelList");
+  }
+
+  public async FriendRequest(nicknameTorequest:String): Promise<boolean>
+  {
+    return await this.Connection.invoke("FriendRequest", nicknameTorequest)
   }
 
 }
