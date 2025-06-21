e7e97e3f04771c0fc9de44461ded6fe976d386d9
Wallace <wallacegomes6877@gmail.com>
2025-06-21 08:51:43 -0300

    style:nav menu for friend requests in main page

--- FrontEnd/src/app/components/default-chat-page/default-chat-page.component.ts
+++ FrontEnd/src/app/components/default-chat-page/default-chat-page.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from "@angular/material/icon";
+import {MatBadgeModule} from "@angular/material/badge";
 
 @Component({
   selector: 'app-default-chat-page',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatBadgeModule
+  ],
   templateUrl: './default-chat-page.component.html',
   styleUrl: './default-chat-page.component.css'
 })
 export class DefaultChatPageComponent {
+  public Solicitacoes = [];
+  public AdicionadosSelecionados = true;
+  public SolicitacoesSelecionados = false;
 
+  public SelectSolicitacoes()
+  {
+    this.SolicitacoesSelecionados = true;
+    this.AdicionadosSelecionados = false;
+  }
+
+  public SelectAdicionados()
+  {
+    this.SolicitacoesSelecionados = false;
+    this.AdicionadosSelecionados = true;
+  }
 }
