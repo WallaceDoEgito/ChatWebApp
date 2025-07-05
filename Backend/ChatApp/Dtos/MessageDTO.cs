551900c00da4b8a04f241538db5d6194e21bd3c4
Wallace <wallacegomes6877@gmail.com>
2025-07-05 20:56:53 -0300

    feat: new fields for messageDto

--- Backend/ChatApp/Dtos/MessageDTO.cs
+++ Backend/ChatApp/Dtos/MessageDTO.cs
@@ -1,3 +1,3 @@
 namespace ChatApp.Dtos;
 
-public record MessageDTO(string UserIdThatSended, string UserNameThatSended, string MessageContent);
+public record MessageDTO(string UserIdThatSended, string UserNameThatSended, string MessageContent, string MessageId, DateTime SendAt, bool Edited);
