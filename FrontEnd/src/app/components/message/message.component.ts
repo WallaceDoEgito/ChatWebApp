3ee0d74a4e5e3ff304f4a5a821edfba144bf6c15
Wallace <wallacegomes6877@gmail.com>
2025-07-11 16:02:53 -0300

    feat: menu for edit and delete messages

--- FrontEnd/src/app/components/message/message.component.ts
+++ FrontEnd/src/app/components/message/message.component.ts
@@ -3,12 +3,19 @@ import {BrazilianDatePipePipe} from "../../pipes/brazilian-date-pipe.pipe";
 import {DatePipe} from "@angular/common";
 import {ChannelDTO} from "../../DTOs/ChannelDTO";
 import {MessageDTO} from "../../DTOs/MessageDTO";
+import {MatIconButton} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenu, MatMenuModule, MatMenuTrigger} from "@angular/material/menu";
 
 @Component({
   selector: 'app-message',
     imports: [
         BrazilianDatePipePipe,
-        DatePipe
+        DatePipe,
+        MatIconButton,
+        MatIconModule,
+        MatMenuTrigger,
+        MatMenuModule,
     ],
   templateUrl: './message.component.html',
   styleUrl: './message.component.css'
@@ -18,14 +25,33 @@ export class MessageComponent {
     MessageToRender = input.required<MessageDTO>()
     IndexMessage = input.required<Number>()
     WhiteImageBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO+ip1sAAAAASUVORK5CYII="
+    MenuIsOpen = false;
+    IsEditting = false;
 
     CompareDates(isoStringOne:string, isoStringTwo:string): boolean
     {
         let DateOne = new Date(isoStringOne);
         let DateTwo = new Date(isoStringTwo);
-
-        console.log(`Comparando: ${isoStringOne} com ${isoStringTwo}`)
-        console.log(`Uma possui dia: ${DateOne.getDate()} e a outra ${DateTwo.getDate()}`)
         return DateOne.getDate() != DateTwo.getDate();
+    }
+
+    MenuOpenned()
+    {
+        this.MenuIsOpen = true;
+    }
+    MenuClosed()
+    {
+        this.MenuIsOpen = false;
+    }
+
+    DeleteMessage()
+    {
+        console.log("Delete Todo")
+        console.log("Going to delete the message: " + this.MessageToRender().messageContent)
+    }
+
+    EditMessage()
+    {
+        console.log("Edit Todo")
     }
 }
