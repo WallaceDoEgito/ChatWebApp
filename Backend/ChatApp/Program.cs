da8f805d77ba42d0ecddff364b6e0864efad4561
Wallace <wallacegomes6877@gmail.com>
2025-07-12 12:13:05 -0300

    fix: service scoped for IMessageService

--- Backend/ChatApp/Program.cs
+++ Backend/ChatApp/Program.cs
@@ -9,7 +9,7 @@ using StackExchange.Redis;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IHasher, BcryptPasswordHasher>();
@@ -17,6 +17,7 @@ builder.Services.AddScoped<AppDbContext>();
 builder.Services.AddScoped<ITokenService, TokenGenerator>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IGetInfo, GetInfoService>();
+builder.Services.AddScoped<IMessageService, MessageModifyService>();
 builder.Services.AddSignalR().AddStackExchangeRedis(builder.Configuration.GetValue<String>("RedisConnectionString")!,
     options =>
     {
